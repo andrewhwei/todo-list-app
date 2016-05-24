b1041c42cc82a87c1202c41fa283676af4d91571
Andrew Wei <andrew.wei92@yahoo.com>
2016-05-23 20:19:35 -0700

    Added a todo list using angular

--- app/assets/javascripts/taskCtrl.js
+++ app/assets/javascripts/taskCtrl.js
@@ -4,7 +4,18 @@
     $scope.tasks = [];
 
     $scope.addTask = function(task) {
-      $scope.tasks.push(task);
+      if ($scope.newTask) {
+        $scope.tasks.push(task);
+        $scope.newTask = null;
+      }
+    }
+
+    $scope.deleteTask = function(index) {
+      $scope.tasks.splice(index, 1);
+    }
+
+    $scope.isPositive = function(task) {
+      return task.indexOf("stupid") == -1;
     }
 
     window.$scope = $scope;
